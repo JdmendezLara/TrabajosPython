184691f7e932e09ca6761a55d3e691907a655556
JdmendezLara <>
2021-04-22 19:59:30 +0000

    reto 4 completado

---
+++ py2_retos1/reto4.py
@@ -0,0 +1,10 @@
+a = int(input("Digite el primer numero: "))
+b = int(input("Digite el segundo numero: "))
+
+if a>b:
+   print("El primero es el mayor")
+elif b>a:
+   print("El segundo es el mayor")
+else :
+   print("Ambos numeros son iguales")
+
