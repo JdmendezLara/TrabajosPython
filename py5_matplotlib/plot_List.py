64ac20c2b324e73902ea0cc94c50b33ce4d84759
JdmendezLara <81332774+JdmendezLara@users.noreply.github.com>
2021-08-11 22:58:57 +0000

    prueba

--- py5_matplotlib/plot_List.py
+++ py5_matplotlib/plot_List.py
@@ -1,15 +1,11 @@
 a = int(input("Digite el numero: "))
 
 def ciclo(a):
-  if a >= 1:
-    while (a =! 1):
+  
+    while (a != 1):
       a = a/2
-      print(b)
-  if a < 1:
-    print("no es posible realizar el programa")
+      print(a)
+    
 
-if a%2 == 0:
-  ciclo
-if a%2 =! 0:
-  a = a*3+1
-  ciclo    
+
+ 
