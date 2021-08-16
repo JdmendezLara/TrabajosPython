2ff15a44304f5e63da7a51351753b256b7c3faf2
JdmendezLara <81332774+JdmendezLara@users.noreply.github.com>
2021-08-16 22:20:50 +0000

    Prueba final y solución

--- py5_matplotlib/plot_List.py
+++ py5_matplotlib/plot_List.py
@@ -1,11 +1,14 @@
-a = int(input("Digite el numero: "))
+listadenumeros = []
+valor = int(input("Digite el numero que sera evaluado: "))
 
-def ciclo(a):
-  
-    while (a != 1):
-      a = a/2
-      print(a)
+while valor != 1:
+  listadenumeros.append(valor)
+  if (valor%2) == 0:
+    valor = valor // 2
+  else:
+    valor = valor*3 + 1
     
+listadenumeros.append(valor)    
 
+print(listadenumeros)
 
- 
